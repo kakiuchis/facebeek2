28e25dd1424c3e15c9dd7d976e647cf149917319
Shusuke Kakiuchi <kakiuchis@gmail.com>
2016-11-27 15:04:44 +0000

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.0'
 set :application, 'facebeek'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/kakiuchis/facebeek'
+set :repo_url, 'https://github.com/kakiuchis/facebeek2'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
