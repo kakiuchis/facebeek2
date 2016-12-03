95c884e834e6a288513185e7db79cdac28e2dc1c
Shusuke Kakiuchi <kakiuchis@gmail.com>
2016-12-03 10:49:09 +0000

    [fix]message.rb空行削除

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,6 @@
 class Message < ActiveRecord::Base
   belongs_to :conversation
   belongs_to :user
-  
   validates_presence_of :body, :conversation_id, :user_id
   def message_time
     created_at.strftime("%Y年%m月%d日 %p %l:%M" )
